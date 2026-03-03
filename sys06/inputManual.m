05fa8d1b360ba311ff3fd488703f9201a2597b0e
Jakefeldmanstarosta <22hfry@queensu.ca>
2026-03-03 09:51:25 -0500

    initial commit course files

---
+++ sys06/inputManual.m
@@ -0,0 +1,10 @@
+function [ f ] = inputManual( t )
+	% Write your own equation here.
+	
+	%---------- This is an example of something you could do to create a discontinuous function.
+
+		f = t^2;
+
+
+	%-----------
+end
